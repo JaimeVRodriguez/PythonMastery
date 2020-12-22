d05ec5cde54b18086189c5ce9c1cccfa25fcd38e
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2020-12-22 14:22:19 -0500

    Complete Section 6-7

---
+++ 1_Primitive_Types/6_number_functions.py
@@ -0,0 +1,7 @@
+import math
+# Google search math module functions
+
+print(round(2.9))
+print(abs(-2.9))
+
+print(math.ceil(2.2))
---
+++ 1_Primitive_Types/7_type_conversion.py
@@ -0,0 +1,7 @@
+# input will result in a String
+x = input("x: ")
+
+# Convert x to int in order to complete equation
+y = int(x) + 1
+
+print(f'x: {x}, y: {y}')
