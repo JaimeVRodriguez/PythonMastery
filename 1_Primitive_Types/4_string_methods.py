a40932fa05a7a035ff4fb254c2a74dbe85787281
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2020-12-21 20:47:24 -0500

    Complete Section 4

---
+++ 1_Primitive_Types/4_string_methods.py
@@ -0,0 +1,12 @@
+course = ' python programming'
+
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.replace('p', 'j'))
+
+# Return index
+print(course.find('pro'))
+# Returns boolean
+print('pro' in course)
