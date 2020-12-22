a035fe7e7d7fb53cb14f1e02ff5428a4444d0e8b
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2020-12-22 14:47:29 -0500

    Complete Section 2.1 - 2.4

---
+++ 2_Control_Flow/3_ternary_operators.py
@@ -0,0 +1,16 @@
+age = 16
+
+# if age >= 18:
+#     print('Eligible')
+# else:
+#     print('Not eligible')
+
+# if age >= 18:
+#     message = 'Eligible'
+# else:
+#     message = 'Not eligible'
+# print(message)
+
+
+message = 'Eligible' if age >= 18 else 'Not eligible'
+print(message)
