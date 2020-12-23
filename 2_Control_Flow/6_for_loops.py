7921e2ff266523c3894035d1d7df4f296dbfae14
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2020-12-23 10:44:22 -0500

    Complete Section 2.5 - 2.8

---
+++ 2_Control_Flow/6_for_loops.py
@@ -0,0 +1,11 @@
+for number in range(3):
+    print('Attempt', number + 1)
+
+# Like split
+# Includes first number and not the second
+for num in range(1, 4):
+    print('Attempt', num)
+
+# Last number is what it is incremented by
+for i in range(1, 10, 2):
+    print('Attempt', i)
---
+++ 2_Control_Flow/8_nested_loops.py
@@ -0,0 +1,7 @@
+for x in range(5):
+    for y in range(3):
+        print(f'{x}, {y}')
+
+# Cycles through outer loop (x)
+# Then does all of inner loop (y)
+# After all inner loop is complete it cycles back around to the outer loop
