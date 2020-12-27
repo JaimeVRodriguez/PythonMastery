dc55e6d559017c83fa52fff618c7e7210978a8ca
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2020-12-27 15:34:06 -0500

    Complete Section 4.1

---
+++ 4_Data_Structures/1_lists.py
@@ -0,0 +1,24 @@
+letters = ['a', 'b', 'c']
+
+# Maxtrix - List of lists
+matrix = [[0, 1], [2, 3]]
+
+zeros = [0] * 5
+# Prints [0, 0 , 0, 0, 0]
+
+combined = zeros + letters
+
+# Just like a split will not print last number in range
+numbers = list(range(20))
+numbers2 = list(range(1, 21))
+
+chars = list('Hello World')
+
+split = letters[1:]
+
+print(combined)
+print(numbers)
+print(numbers2)
+print(split)
+print(chars)
+print(len(chars))
