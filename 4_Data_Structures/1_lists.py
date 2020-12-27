d503b60cfab03ab9f7d3870fa41dc332ec7646da
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2020-12-27 15:41:14 -0500

    Complete Section 4.1

--- 4_Data_Structures/1_lists.py
+++ 4_Data_Structures/1_lists.py
@@ -1,4 +1,4 @@
-letters = ['a', 'b', 'c']
+letters = ['a', 'b', 'c', 'd']
 
 # Maxtrix - List of lists
 matrix = [[0, 1], [2, 3]]
@@ -14,11 +14,20 @@ numbers2 = list(range(1, 21))
 
 chars = list('Hello World')
 
+first = letters[0]
 split = letters[1:]
+alternate = numbers[::2]
+reverse = numbers2[::-1]
 
+print('-- Printing')
 print(combined)
 print(numbers)
 print(numbers2)
+print('-- Accessing')
+print(first)
 print(split)
+print(alternate)
+print(reverse)
+print()
 print(chars)
 print(len(chars))
