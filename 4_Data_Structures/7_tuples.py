ada6b2f6ca422eb88bc71de170c56c4435f0a81d
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2020-12-28 18:58:07 -0500

    Update

--- 4_Data_Structures/7_tuples.py
+++ 4_Data_Structures/7_tuples.py
@@ -4,3 +4,7 @@ print(point[0])
 
 point = (1, 2) + (3, 4)
 print(point)
+
+# Basically do anything with as with a list
+# Just cant modify in any way
+# Use when you don't want your "list" to be changed on accident
