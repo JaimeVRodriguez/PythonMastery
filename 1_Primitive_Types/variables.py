8c3d4a9426c32421205f7c36b3f24d43b325817d
Jaime Rodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-08-17 23:08:51 -0400

    Lesson 1.1

---
+++ 1_Primitive_Types/variables.py
@@ -0,0 +1,17 @@
+
+#! Whole numbers are integers aka int
+students_count = 1000
+
+#! "Decimal" numbers are float
+rating = 4.99
+
+#! Boolean has two values "true" or "false"
+is_published = False
+
+#! String is a combination of characters, surrounded by quotes
+course_name = 'Python'
+
+print(students_count)
+
+
+
