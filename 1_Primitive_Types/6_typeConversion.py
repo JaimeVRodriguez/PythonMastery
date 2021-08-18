d58b1617add7d424190102fc01a655fe001b3907
Jaime Rodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-08-18 09:38:12 -0400

    Lesson 1.6

---
+++ 1_Primitive_Types/6_typeConversion.py
@@ -0,0 +1,14 @@
+
+#! input takes user input
+x = input("x: ")
+y = int(x) + 1
+
+print(f"x: {x}, y: {y}")
+
+# float(x)
+# bool(x)
+
+#! Following are consider False bool
+# "" (empty string)
+# 0
+# None
