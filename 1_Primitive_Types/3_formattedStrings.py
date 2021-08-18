57608d60a05b357a0e0f9bb9caa7ffd729bd4861
Jaime Rodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-08-17 23:28:32 -0400

    Lesson 1.3

---
+++ 1_Primitive_Types/3_formattedStrings.py
@@ -0,0 +1,11 @@
+
+first = "Jaime"
+last = "Rodriguez"
+#! Concactinate (combine) words using "+"
+full = first + " " + last
+
+#! Formated using "f"
+formatted_full = f"{first} {last}"
+
+print(full)
+print(formatted_full)
