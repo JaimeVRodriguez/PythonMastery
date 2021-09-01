dc9daf07de5e9832826d808a39113e2d0c7b3e46
Jaime Rodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-09-01 13:09:06 -0400

    Lesson 4.2

---
+++ 4_Data_Structures/2_accessingItems.py
@@ -0,0 +1,13 @@
+
+letters = ["a", "b", "c", "d"]
+#! Change items in list
+letters[0] = "A"
+
+#! Lists start with index 0
+print(letters[0])
+#! -1 is last item (or goes backwards)
+print(letters[-1])
+
+print(letters[0:2])
+
+
