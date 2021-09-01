c29ab0c4c2c50f233707f091a7fda7ec785d595c
Jaime Rodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-09-01 13:25:29 -0400

    Lesson 4.4

---
+++ 4_Data_Structures/4_loopingLists.py
@@ -0,0 +1,10 @@
+
+letters = ["a", "b", "c"]
+
+#! For every "letter" in "letters" list prin the letter
+for letter in letters:
+    print(letter)
+
+#! Enumerate will give a tuple
+for index, letter in enumerate(letters):
+    print(index, letter)
