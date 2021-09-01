c1e9a4c55097ae0b10f4379eb56f0665cfa8e37a
Jaime Rodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-09-01 12:56:47 -0400

    Lesson 4.1

---
+++ 4_Data_Structures/1_lists.py
@@ -0,0 +1,15 @@
+
+#! Defined with []
+letters = ["a", "b", "c"]
+
+#! List of lists is a matrix
+matrix = [[0, 1], [2, 3]]
+
+# List of numbers without typing all numbers
+numbers = list(range(20))
+print(numbers)
+
+# Strings are iterable and can be made into a list
+chars = list("Hello")
+print(chars)
+print(len(chars))
